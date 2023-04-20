92c6a111831e2aeed6559fe851bb47795b9643e5
nkurudavid <nkurudavid44@gmail.com>
2023-04-20 12:46:28 +0200

    modified signals

--- renting/apps.py
+++ renting/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RentingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'renting'
+
+    def ready(self):
+        import renting.signals.handler
