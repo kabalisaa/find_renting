e6da1c0a87f8f39c75ae1a57414916c239b55bee
nkurudavid <nkurudavid44@gmail.com>
2023-04-24 15:21:04 +0200

    static files

--- backend/settings.py
+++ backend/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -32,11 +32,13 @@ SYSTEM_APPS=[
 
 THIRD_PARTY_APPS=[
     'rest_framework',
+    'whitenoise.runserver_nostatic',
     'phonenumber_field',
     'corsheaders',
     'django_filters',
     'drf_spectacular',
     'drf_spectacular_sidecar',
+    
 ]
 
 PROJECT_APPS=[
@@ -56,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -134,6 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # storage for files
 MEDIA_URL = '/media/'
